46ef7de78690ed2bd95eee5ef43f92ba5990aae1
Michael Oberman <obermanmichael6@gmail.com>
2025-05-26 15:07:59 +0300

    Refactor client/server logic and add HTTP server class\

--- src/org/ober6/charm/back/CharmBackClientRunner.java
+++ src/org/ober6/charm/back/CharmBackClientRunner.java
@@ -6,26 +6,38 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.URI;
 import java.net.http.HttpResponse;
-import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CharmBackClientRunner {
     public static void main(String[] args) throws IOException, InterruptedException {
         try (HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build()) {
 
-            HttpRequest request = HttpRequest.newBuilder(URI.create("http://yandex.ru"))
-                    .setHeader("My-Token", "zxcvbnm")
-                    .POST(HttpRequest.BodyPublishers.ofString("dsa")).
-                    build();
+//            HttpRequest request = HttpRequest.newBuilder(URI.create("http://yandex.ru"))
+//                    .setHeader("My-Token", "zxcvbnm")
+//                    .POST(HttpRequest.BodyPublishers.ofString("dsa")).
+//                    build();
 
-           HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/profile?id=1"))
+                    .GET()
+                    .build();
 
-           Map<String, List<String>> map = httpResponse.headers().map();
+//           HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+//            Map<String, List<String>> map = httpResponse.headers().map();
+//            System.out.println(httpResponse.statusCode());
+//            System.out.println();
+//            System.out.println(map);
+//            System.out.println(httpResponse.body());
 
-            System.out.println(httpResponse.statusCode());
-            System.out.println();
-            System.out.println(map);
-            System.out.println(httpResponse.body());
+            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            HttpClient.Version version = response.version();
+            int statusCode = response.statusCode();
+
+            String headers = response.headers().map().entrySet().stream().map(Object::toString).collect(
+                    Collectors.joining()
+            );
+            String body = new String(response.body());
+            System.out.println(version + " " + statusCode + "\n" + headers + "\n\n" + body);
         }
     }
 }
---
+++ src/org/ober6/charm/back/CharmHttpServer.java
@@ -0,0 +1,55 @@
+package org.ober6.charm.back;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//TODO : add parameter to constructor to submit controller from the CharmBackServerRunner and call it's method, accroding to request ( GET -> .findByAd, POST -> .add etc...)
+//TODO: add some another controller
+public class CharmHttpServer {
+    private ExecutorService executorService;
+
+    public CharmHttpServer(int poolSize) {
+        this.executorService = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                Socket connection = serverSocket.accept();
+                System.out.println("-------- Client connect-----------");
+                executorService.submit(() -> processConnection(connection));
+
+            }
+        }
+    }
+
+    private void processConnection(Socket connection){
+        try (connection;
+             BufferedReader rqReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             DataOutputStream rsStream = new DataOutputStream(connection.getOutputStream())
+        ) {
+            while(!rqReader.ready());
+
+            while(rqReader.ready()){
+                System.out.println(rqReader.readLine());
+            }
+
+            byte[] body = "Hi from CharmServer".getBytes();
+            byte[] startString = "HTTP/1.1 200 OK\n".getBytes();
+            byte[] headers = "Content-Type: text/plain\nContent-length: %s\n\n".formatted(body.length).getBytes();
+
+            rsStream.write(startString);
+            rsStream.write(headers);
+            rsStream.write(body);
+            System.out.println("-------- Client disconnect-----------");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
