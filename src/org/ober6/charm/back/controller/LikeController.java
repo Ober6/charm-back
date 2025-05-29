5cecbb2fc097bf433e16988a60506c728b87a435
Michael Oberman <obermanmichael6@gmail.com>
2025-05-29 16:01:57 +0300

    Removed CharmBackClientRunner, CharmBackServerRunner, and CharmHttpServer classes; migrated application to use Tomcat server.

--- src/org/ober6/charm/back/controller/LikeController.java
+++ src/org/ober6/charm/back/controller/LikeController.java
@@ -1,8 +1,19 @@
 package org.ober6.charm.back.controller;
 
-public class LikeController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-    public int count(){
-        return 10;
+import java.io.IOException;
+
+@WebServlet("/like")
+public class LikeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("10");
     }
+
 }
